280417182f4b1d53e0e9a37bd241ef5636ecc2d2
aleksgrinko <118903811+aleksgrinko@users.noreply.github.com>
2023-04-17 13:16:48 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 apt update
-apt upgrade -y
+# apt upgrade -y
 apt install -y \
-        python3-pip \
+        git
+        python3-pip 
 python3 -m pip install -U pip
+git checkout 72
 ls -la > /app/testfile
