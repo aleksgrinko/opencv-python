2ef71f8425a132d24ea3d7ec59e1adb19aba0d24
aleksgrinko <118903811+aleksgrinko@users.noreply.github.com>
2023-04-18 11:09:28 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -41,9 +41,11 @@ apt install -y \
 python3 -m pip install -U pip
 git clone --recursive https://github.com/opencv/opencv-python.git
 cd opencv-python
+git checkout 72
 export CMAKE_BUILD_PARALLEL_LEVEL=$(nproc)
 export CMAKE_ARGS="-D BUILD_opencv_world=OFF -DCPU_BASELINE=SSE4_2 -D CUDA_FAST_MATH=OFF -D OPENCV_DNN_CUDA=OFF -D WITH_CUBLAS=ON -D WITH_CUDA=OFF -D WITH_OPENVINO=OFF -D WITH_CUDNN=OFF -D EIGEN_INCLUDE_PATH=/usr/include/eigen3 -D WITH_OPENCL=ON -D WITH_OPENMP=OFF -D WITH_TBB=ON -D WITH_1394=OFF -D OPENCV_ENABLE_NONFREE=OFF -D OPENCV_GENERATE_PKGCONFIG=ON -D WITH_GSTREAMER=ON -D WITH_LIBV4L=ON -D WITH_OPENGL=OFF -D WITH_GTK=OFF -D INSTALL_CREATE_DISTRIB=ON -D BUILD_SHARED_LIBS=OFF"
 export ENABLE_CONTRIB=1
 export ENABLE_HEADLESS=1
+cd opencv-python
 pip wheel . --verbose --wheel-dir=/app/wheels 
 ls -la wheels/ > /app/testfile
