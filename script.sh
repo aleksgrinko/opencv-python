987cff60e91909cf628bf08f35d46d4b5b416b79
aleksgrinko <118903811+aleksgrinko@users.noreply.github.com>
2023-07-31 11:55:06 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -46,6 +46,13 @@ latest_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
 git checkout $latest_tag
 export CMAKE_BUILD_PARALLEL_LEVEL=$(nproc)
 export CMAKE_ARGS="-D BUILD_opencv_world=OFF -DCPU_BASELINE=SSE4_2 -D CUDA_FAST_MATH=OFF -D OPENCV_DNN_CUDA=OFF -D WITH_CUBLAS=ON -D WITH_CUDA=OFF -D WITH_OPENVINO=OFF -D WITH_CUDNN=OFF -D EIGEN_INCLUDE_PATH=/usr/include/eigen3 -D WITH_OPENCL=ON -D WITH_OPENMP=OFF -D WITH_TBB=ON -D WITH_1394=OFF -D OPENCV_ENABLE_NONFREE=OFF -D OPENCV_GENERATE_PKGCONFIG=ON -D WITH_GSTREAMER=ON -D WITH_LIBV4L=ON -D WITH_OPENGL=OFF -D WITH_GTK=OFF -D INSTALL_CREATE_DISTRIB=ON -D BUILD_SHARED_LIBS=OFF"
+pip wheel . --wheel-dir=/app/wheels
+export ENABLE_CONTRIB=1 
+ls -la
+pwd
+git clean -fdx
+pwd
+pip wheel . --wheel-dir=/app/wheels
+ls -la
 export ENABLE_CONTRIB=1
 export ENABLE_HEADLESS=1
-pip wheel . --verbose --wheel-dir=/app/wheels 
